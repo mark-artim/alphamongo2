8e4e9d2936d37a76f74e55363e88dd790e71036f
Mark Artim <mark.artim@gmail.com>
2023-03-28 15:48:54 -0500

    pre-deployment on heroku clean up

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const path = require('path')
 
 console.log('environment    ', process.env.NODE_ENV)
 console.log('PORT    ', process.env.PORT)
@@ -23,6 +24,21 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/products', require('./routes/productRoutes'))
 app.use('/api/customers', require('./routes/customerRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// --------------- deployment ------------------
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html'));
+
+    })
+} else {
+    app.get('/', (req,res) => {
+        res.send('API is running...')
+    })
+}
 
 app.use(errorHandler)
 
